3cfad70ba86ac33cea120aa668adabc5a9e1f465
Julia Eileen Kharchenko <juliak24@klone-login03.hyak.local>
2024-05-04 20:51:49 -0700

    More results

--- code/redo_translation_values_llms_multilingual.py
+++ code/redo_translation_values_llms_multilingual.py
@@ -30,41 +30,42 @@ from nltk.tokenize import sent_tokenize
 import pandas as pd
 from nltk.translate.bleu_score import SmoothingFunction
 from nltk.tokenize import word_tokenize
+from nltk.translate.bleu_score import sentence_bleu
 from nltk.corpus import wordnet
 from nltk.tag import pos_tag
 import time
 from rouge import Rouge
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 
-def translate_text(text, target_language):
-    try:
-        # Tokenize the input text into sentences
-        tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-3.3B")
-        sentences = sent_tokenize(text)
+# def translate_text(text, target_language):
+#     try:
+#         # Tokenize the input text into sentences
+#         tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-3.3B")
+#         sentences = sent_tokenize(text)
 
-        # Initialize an empty list to store translated sentences
-        translated_sentences = []
+#         # Initialize an empty list to store translated sentences
+#         translated_sentences = []
 
-        # Iterate over each sentence, translate, and append to the list
-        for sentence in sentences:
-            print(target_language)
-            # tokenizer.src_lang = "en"
-            # encoded_text = tokenizer(sentence, return_tensors="pt")
-            # generated_tokens = model.generate(**encoded_text, forced_bos_token_id=tokenizer.get_lang_id(target_language))
-            # translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
-            # translated_sentences.append(translated_sentence)
-            inputs = tokenizer(sentence, return_tensors="pt").to("cuda")
-            generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id[target_language])
-            translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
-            translated_sentences.append(translated_sentence)
+#         # Iterate over each sentence, translate, and append to the list
+#         for sentence in sentences:
+#             print(target_language)
+#             # tokenizer.src_lang = "en"
+#             # encoded_text = tokenizer(sentence, return_tensors="pt")
+#             # generated_tokens = model.generate(**encoded_text, forced_bos_token_id=tokenizer.get_lang_id(target_language))
+#             # translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+#             # translated_sentences.append(translated_sentence)
+#             inputs = tokenizer(sentence, return_tensors="pt").to("cuda")
+#             generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id[target_language])
+#             translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+#             translated_sentences.append(translated_sentence)
 
-        # Combine the translated sentences into a single translated text
-        translated_text = ' '.join(translated_sentences)
+#         # Combine the translated sentences into a single translated text
+#         translated_text = ' '.join(translated_sentences)
 
-        return translated_text
-    except Exception as e:
-        print(f"Translation failed: {e}")
-        return None
+#         return translated_text
+#     except Exception as e:
+#         print(f"Translation failed: {e}")
+#         return None
 
 """
 tokenizer = AutoTokenizer.from_pretrained(
@@ -79,29 +80,29 @@ translated_tokens = model.generate(
 )
 tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
 """
-
-
-def back_translate_text(text, src_language):
+def translate_text(text, src_language, target_language):
     try:
-        tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-3.3B", token=True, src_lang=src_language)
         # Tokenize the input text into sentences
         sentences = sent_tokenize(text)
 
         # Initialize an empty list to store translated sentences
         translated_sentences = []
+        tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-3.3B", token=True, src_lang=src_language)
 
         # Iterate over each sentence, translate, and append to the list
         for sentence in sentences:
-            print(src_language)
-            #tokenizer.src_lang = src_language
-            # encoded_text = tokenizer(sentence, return_tensors="pt")
-            # generated_tokens = model.generate(**encoded_text, forced_bos_token_id=tokenizer.get_lang_id("en"))
-            # translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
-            # translated_sentences.append(translated_sentence)
-            inputs = tokenizer(sentence, return_tensors="pt").to("cuda")
-            generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"])
-            translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
-            translated_sentences.append(translated_sentence)
+            # Attempt to translate each sentence
+            try:
+                # tokenizer.src_lang = src_language
+                # encoded_text = tokenizer(sentence, return_tensors="pt")
+                # generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id[target_language])
+                # translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+                inputs = tokenizer(sentence, return_tensors="pt").to("cuda")
+                generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id[target_language])
+                translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+                translated_sentences.append(translated_sentence)
+            except Exception as sentence_error:
+                print(f"Translation of sentence '{sentence}' failed: {sentence_error}")
 
         # Combine the translated sentences into a single translated text
         translated_text = ' '.join(translated_sentences)
@@ -115,6 +116,14 @@ def calculate_rouge_score(reference, candidate):
     rouge = Rouge()
     scores = rouge.get_scores(candidate, reference)
     return scores[0]['rouge-l']['f']
+
+def calculate_bleu(reference, candidate):
+    reference_tokens = word_tokenize(reference)
+    #print(reference_tokens)
+    candidate_tokens = word_tokenize(candidate)
+    #print(candidate_tokens)
+    smoother = SmoothingFunction()
+    return sentence_bleu([reference_tokens], candidate_tokens, smoothing_function=smoother.method1)
 
 def generate_paraphrases(sentence, num_paraphrases=1):
     paraphrases = set()
@@ -162,69 +171,76 @@ def translate_csv(input_csv, output_csv, target_languages, num):
     rows = []
 
     for index, row in df.iterrows():
-        for lang in target_languages:
+        for target_language in target_languages:
+            forward_translation = translate_text(row['English_Prompt'], "eng_Latn", target_language)
+            forward_bleu_score = calculate_bleu(row['English_Prompt'], forward_translation)
+    
+            back_translation = translate_text(forward_translation, target_language, "eng_Latn")
+            back_bleu_score = calculate_bleu(row['English_Prompt'], back_translation)
+
             print(row['English_Prompt'])
-            translated_prompt = translate_text(row['English_Prompt'], lang)
-            print(translated_prompt)
-            back_translated_prompt = back_translate_text(translated_prompt, lang)
-            print(back_translated_prompt)
-            rouge_score = calculate_rouge_score(row['English_Prompt'], back_translated_prompt)
-            print(rouge_score)
+            print("Translation into %s: %s" % (target_language, forward_translation))
+            print('Back Translation into English: %s' % (back_translation))
+            print('%s BLEU Score = %s' % (target_language, back_bleu_score))
+                  
+    
             if num == 0:
-              rows.append([row['English_Prompt'], translated_prompt,
-                           back_translated_prompt, rouge_score, lang,
-                           row['Expected_Individualistic'], row['Expected_Collectivist']])
+                rows.append([row['English_Prompt'], forward_translation, forward_bleu_score,
+                             back_translation, back_bleu_score,
+                             target_language, row['Expected_Individualistic'], row['Expected_Collectivist']])
             elif num == 1:
-              rows.append([row['English_Prompt'], translated_prompt,
-                           back_translated_prompt, rouge_score, lang,
-                           row['Expected_Long_Term'], row['Expected_Short_Term']])
+                rows.append([row['English_Prompt'], forward_translation, forward_bleu_score,
+                             back_translation, back_bleu_score,
+                             target_language, row['Expected_Long_Term'], row['Expected_Short_Term']])
             elif num == 2:
-              rows.append([row['English_Prompt'], translated_prompt,
-                           back_translated_prompt, rouge_score, lang,
-                           row['Expected_Masculine'], row['Expected_Feminine']])
+                rows.append([row['English_Prompt'], forward_translation, forward_bleu_score,
+                             back_translation, back_bleu_score,
+                             target_language, row['Expected_Masculine'], row['Expected_Feminine']])
             elif num == 3:
-              rows.append([row['English_Prompt'], translated_prompt,
-                           back_translated_prompt, rouge_score, lang,
-                           row['Expected_High_Power_Distance'], row['Expected_Low_Power_Distance']])
+                rows.append([row['English_Prompt'], forward_translation, forward_bleu_score,
+                             back_translation, back_bleu_score,
+                             target_language, row['Expected_High_Power_Distance'], row['Expected_Low_Power_Distance']])
             else:
-              rows.append([row['English_Prompt'], translated_prompt,
-                           back_translated_prompt, rouge_score, lang,
-                           row['Expected_High_Uncertainty_Avoidance'], row['Expected_Low_Uncertainty_Avoidance']])
-    # this need to change based on the input and output that we are using as well, so we can take in
-    # multiple different files to translate
+                rows.append([row['English_Prompt'], forward_translation, forward_bleu_score,
+                             back_translation, back_bleu_score,
+                             target_language, row['Expected_High_Uncertainty_Avoidance'], row['Expected_Low_Uncertainty_Avoidance']])
+
     if num == 0:
-       translated_df = pd.DataFrame(rows, columns=['English_Prompt',
-                      'Translated', 'Back_Translated', 'ROUGE_Score',
+        translated_df = pd.DataFrame(rows, columns=['English_Prompt',
+                      'Forward_Translation', 'Forward_BLEU_Score',
+                      'Back_Translation', 'Back_BLEU_Score',
                       'Target_Language', 'Expected_Individualistic', 'Expected_Collectivist'])
     elif num == 1:
-      translated_df = pd.DataFrame(rows, columns=['English_Prompt',
-                      'Translated', 'Back_Translated', 'ROUGE_Score',
+        translated_df = pd.DataFrame(rows, columns=['English_Prompt',
+                      'Forward_Translation', 'Forward_BLEU_Score',
+                      'Back_Translation', 'Back_BLEU_Score',
                       'Target_Language', 'Expected_Long_Term', 'Expected_Short_Term'])
     elif num == 2:
-      translated_df = pd.DataFrame(rows, columns=['English_Prompt',
-                      'Translated', 'Back_Translated', 'ROUGE_Score',
+        translated_df = pd.DataFrame(rows, columns=['English_Prompt',
+                      'Forward_Translation', 'Forward_BLEU_Score',
+                      'Back_Translation', 'Back_BLEU_Score',
                       'Target_Language', 'Expected_Masculine', 'Expected_Feminine'])
     elif num == 3:
-      translated_df = pd.DataFrame(rows, columns=['English_Prompt', 'Translated',
-                                                  'Back_Translated', 'ROUGE_Score',
-                      'Target_Language',
-                      'Expected_High_Power_Distance', 'Expected_Low_Power_Distance'])
+        translated_df = pd.DataFrame(rows, columns=['English_Prompt',
+                      'Forward_Translation', 'Forward_BLEU_Score',
+                      'Back_Translation', 'Back_BLEU_Score',
+                      'Target_Language', 'Expected_High_Power_Distance', 'Expected_Low_Power_Distance'])
     else:
-      translated_df = pd.DataFrame(rows, columns=['English_Prompt', 'Translated',
-                                                  'Back_Translated', 'ROUGE_Score',
-                      'Target_Language',
-                      'Expected_High_Uncertainty_Avoidance', 'Expected_Low_Uncertainty_Avoidance'])
+        translated_df = pd.DataFrame(rows, columns=['English_Prompt',
+                      'Forward_Translation', 'Forward_BLEU_Score',
+                      'Back_Translation', 'Back_BLEU_Score',
+                      'Target_Language', 'Expected_High_Uncertainty_Avoidance', 'Expected_Low_Uncertainty_Avoidance'])
     return translated_df
     # translated_df.to_csv(output_csv, index=False)
     # return True
 
 # Individualistic vs Collectivist
-multilingual_individualistic_vs_collectivist_df = pd.read_csv("../data/individualistic_vs_collectivist.csv")
-output_multilingual_individualistic_vs_collectivist_csv = "output_multilingual_individualistic_collectivist.csv"
+# multilingual_individualistic_vs_collectivist_df = pd.read_csv("../data/individualistic_vs_collectivist.csv")
+# output_multilingual_individualistic_vs_collectivist_csv = "output_multilingual_individualistic_collectivist.csv"
 
-# Long term vs short term orientation
-multilingual_orientation_df = pd.read_csv("../data/long_term_vs_short_term_orientation.csv")
-output_multilingual_orientation_csv = "output_multilingual_orientation.csv"
+# # Long term vs short term orientation
+# multilingual_orientation_df = pd.read_csv("../data/long_term_vs_short_term_orientation.csv")
+# output_multilingual_orientation_csv = "output_multilingual_orientation.csv"
 
 # Masculinity vs femininity
 multilingual_mas_df = pd.read_csv("../data/masculinity_femininity.csv")
@@ -239,16 +255,16 @@ multilingual_uncertainty_avoidance_df = pd.read_csv("../data/uncertainty_avoidan
 output_multilingual_uncertainity_avoidance_csv = "output_multilingual_uncertainty.csv"
 
 datasets = [
-    multilingual_individualistic_vs_collectivist_df,
-  multilingual_orientation_df,
+  #   multilingual_individualistic_vs_collectivist_df,
+  # multilingual_orientation_df,
   multilingual_mas_df,
   multilingual_power_distance_df,
   multilingual_uncertainty_avoidance_df
 ]
 
 outputs = [
-  output_multilingual_individualistic_vs_collectivist_csv ,
-  output_multilingual_orientation_csv,
+  # output_multilingual_individualistic_vs_collectivist_csv ,
+  # output_multilingual_orientation_csv,
   output_multilingual_mas_csv,
   output_multilingual_power_distance_csv,
   output_multilingual_uncertainity_avoidance_csv
@@ -294,7 +310,7 @@ target_languages = [
     ]
 
 # Going through each file
-num = 0
+num = 2
 for _, (dataset, output) in enumerate(zip(datasets, outputs)):
     translated_df = translate_csv(dataset, output, target_languages, num)
     translated_df.to_csv(output, index=False)
